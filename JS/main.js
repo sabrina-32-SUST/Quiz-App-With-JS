e12a8b676248dc035c328616c312ac08875b1ed6
Sabrina <sanutaayvi@gmail.com>
2021-02-28 15:00:22 +0600

    create questions

--- JS/main.js
+++ JS/main.js
@@ -46,3 +46,13 @@ function showScores() {
    };
 
 
+   // create questions
+var questions = [
+    new Question("Which one is not an object oriented programming language?", ["Java", "C#","C++", "C"], "C"),
+    new Question("Which language is used for styling web pages?", ["HTML", "JQuery", "CSS", "XML"], "CSS"),
+    new Question("There are ____ main components of object oriented programming.", ["1", "6","2", "4"], "4"),
+    new Question("Which language is used for web apps?", ["PHP", "Python", "Javascript", "All"], "All"),
+    new Question("MVC is a ____.", ["Language", "Library", "Framework", "All"], "Framework")
+];
+
+
